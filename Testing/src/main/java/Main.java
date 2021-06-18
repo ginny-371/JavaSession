830ad65601c7ed42c1a735101c37ef5b936aae7e
ginny13 <dong.tong@onemount.com>
2021-06-18 18:45:12 +0700

    Nop bai buoi 16

--- Testing/src/main/java/Main.java
+++ Testing/src/main/java/Main.java
@@ -3,83 +3,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws CheckingException {
-        ArrayList<Product> listProduct = new ArrayList<>();
-        listProduct.add(new Product(1, "Thịt Gà", 10, 45.000));
-        listProduct.add(new Product(2, "Thịt lợn", 10, 90.000));
-        listProduct.add(new Product(3, "Thịt Bò", 10, 200.000));
-        //In ra các sản phẩm
-        System.out.println("in các sản phẩm");
-        for(Product x:listProduct){
-            System.out.println(x);
-        }
-        //Tìm kiếm sản phẩm theo tên
-        System.out.println("Tìm kiếm sản phẩm thịt gà");
-        for (int i = 0; i < listProduct.size(); i++) {
-            if (listProduct.get(i).productName=="Thịt Gà") {
-                System.out.println(listProduct.get(i));
-            }
-        }
-        //Sửa sản phẩm
-        System.out.println("Sửa sản phẩm");
-        for(int i = 0; i<listProduct.size(); i++){
-            if(listProduct.get(i).productID ==1){
-//                listProduct.get(i).productName="Thịt gà";
-                listProduct.set(i,new Product(listProduct.get(i).productID,"Thịt gà",listProduct.get(i).quantity,listProduct.get(i).price));
-            }
-        }
-        System.out.println("In danh sách sản phẩm sau khi edit");
-        for(Product x:listProduct){
-            System.out.println(x);
-        }
-        //Xóa sản phẩm
-        for(int i = 0; i<listProduct.size(); i++){
-            if(listProduct.get(i).productID ==1){
-                listProduct.remove(listProduct.get(i));
-            }
-        }
-        System.out.println("in thông tin sau khi Xóa");
-        for(Product x:listProduct){
-            System.out.println(x);
-        }
-
-        ArrayList<Staff> listStaff = new ArrayList<>();
-        listStaff.add(new Staff("NV1","Dong",29, "Hai Bà Trưng, Hà Nội", "0122123434"));
-        listStaff.add(new Staff("NV2","Ngoc",25,"Hoàng Mai","0922123434"));
-        listStaff.add(new Staff("NV3","Brian",25,"Vĩnh Tuy, Hà Nội","0122123434"));
-
-        for(Staff x:listStaff){
-            System.out.println(x);
-        }
         Scanner sc = new Scanner(System.in);
-        Boolean flag = false;
-        System.out.println("Nhập tên nhân viên bạn muốn tìm kiếm: ");
-        String name = sc.nextLine();
-        for(int i = 0; i<listStaff.size(); i++){
-            if(listStaff.get(i).name.contains(name)) {
-                listStaff.set(i, new Staff(listStaff.get(i).id, "Lan", 29, "Hai Bà Trưng, Hà Nội", "0122123434"));
-                flag = true;
-            }
-        }
-        if (flag==false){
-            System.out.println("Không tìm thấy nhân viên");
-        }
+        System.out.println("Nhập danh sách bạn muốn thao tác: ");
+        System.out.println("Staff: Ấn 1");
+        System.out.println("Product: Ấn 2");
+        int bt = sc.nextInt();
+switch (bt) {
+    case 1: {
+        ListStaff staff = new ListStaff();
+        staff.addStaff();
+        System.out.println("In danh sách sau khi add: ");staff.printStaff();
+        staff.updateStaff();
+        staff.removeStaff();
+        break;
+    }
+    case 2: {
+        System.out.println();
+        ListProducts products = new ListProducts();
+        products.addProduct();
+        System.out.println("In danh sách sau khi add: ");products.printProduct();
+        products.updateProduct();
+        System.out.println("In danh sách sau khi update: ");products.printProduct();
+        products.removeProduct();
+        System.out.println("In danh sách sau khi xóa: ");products.printProduct();
+        break;
+    } default:
+        System.out.println("Nhập sai rồi");
+}
 
-        boolean flag2 = false;
-        System.out.println("Nhập id bạn muốn tìm kiếm: ");
-        String id = sc.nextLine();
-        for(int i = 0; i<listStaff.size(); i++){
-            if(listStaff.get(i).id.contains(id)) {
-                listStaff.remove(listStaff.get(i));
-                flag2 = true;
-            }
-        }
-        if (flag2==false){
-            System.out.println("Không tìm thấy nhân viên");
-        }
-        System.out.println("In danh sách sau khi xóa nhân viên id= "+id);
-        for(Staff x:listStaff){
-            System.out.println(x);
-        }
+
 
             //Bai 1
 //        BMIException tc = new BMIException();
